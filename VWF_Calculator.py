4b969cf882ffa60bd8f77a89784d642d6d815355
Bunkai9448 <71356234+Bunkai9448@users.noreply.github.com>
2024-08-18 04:57:42 +0200

    Hopefully getting close for the front-end

--- VWF_Calculator.py
+++ VWF_Calculator.py
@@ -6,52 +6,58 @@
 
 import sys # will allow us to handle the application's termination and exit status through the exit() function
 from PyQt5.QtWidgets import * # Import QApplication and all the required widgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
-# Displayed Window
-class Window(QDialog):
-
+class Window(QWidget):
 	def __init__(self):
-		super().__init__()
+		super(Window, self).__init__()
 		
 		self.setWindowTitle("Variable Width Font Calculator")
-		self.setGeometry(100, 100, 400, 200)  # (x, y, width, height)
+		self.setGeometry(100, 100, ScreenWidth, ScreenHeight)  # (x, y, width, height)
 		
-		layout = QVBoxLayout()
-		
+		layout = QVBoxLayout(self)
+
 		# Create a QLineEdit widget
-		self.label = QLabel("ScreenSize: " + str(ScreenResolution) + "  ||  TileSize: " + str(TileSize) ) # Display Game's screen Resolution and Font Game's Resolution
-		layout.addWidget(self.label)			
+		self.label = QLabel("ScreenSize: " + str(ScreenWidth) + "x" + str(ScreenHeight) + "  ||  TileSize: " + str(TileSize) ) # Display Game's screen Resolution and Font Game's Resolution
+		self.label.setFixedHeight(10)
+		layout.addWidget(self.label)	
+		
 		
 		# Create a QLineEdit widget
 		self.line_edit = QLineEdit(self)
 		layout.addWidget(self.line_edit)
-		
+
+
 		# Create a QLabel widget
-		self.label = QLabel("Display: ", self)
+		self.label = QLabel("", self)
 		layout.addWidget(self.label)
+		self.label.setStyleSheet(" background-image : url(Background);")
 		
 		# Connect the textChanged signal to the update_label slot
 		self.line_edit.textChanged.connect(self.update_label)
 		
-		self.setLayout(layout)
 		
 	# Slot function to update the label text
-	def update_label(self):
+	def update_label(self,Dialogue):
 		text = self.line_edit.text()
-		self.label.setText("Display: " + text)
-
-
+		self.label.setText(text)
+		self.label.setStyleSheet("background-image : url(Background); font: TileSize px;")
+		
 if __name__ == "__main__":
 
-	ScreenResolution = 18
+	ScreenWidth = 280
+	ScreenHeight = 280
 	TileSize = 8
-
+	Background = 'Background.png'
+	
 	# Instance of QApplication to use
 	app = QApplication([])
 	# Our application's GUI
 	window = Window()
 	# Show application's GUI in screen
 	window.show()
+	
 	# Run application's event loop
 	sys.exit(app.exec())
